b53045137b8e4e6c0e7f4b27540050e8074f75b6
rogpe <prog_x@hotmail.com>
2019-08-10 22:01:05 -0300

    upload files with Multer, Express and EJS

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now()+'-'+file.originalname)
+    }
+})
+const upload = multer( { storage })
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+app.post('/', upload.single('files'), (req, res) => {
+    console.log(req.body, req.file)
+    res.send('OK')
+})
+
+app.listen(3000, () => console.log('online...'))
